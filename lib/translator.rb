827190e58034f3754b12b0f6d83f12132266d789
Deborah Savage <savaged725@yahoo.com>
2019-07-16 21:12:45 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,15 @@
-# require modules here
+
 
 def load_library
-  # code goes here
+  load_library("./lib/emoticons.yml")
 end
 
 def get_japanese_emoticon
-  # code goes here
+  puts angel
+  
+  end
+
+  
 end
 
 def get_english_meaning
